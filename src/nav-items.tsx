16cf8b7275c2219a6a6ade4522cef98cd21d5401
Nikolino98 <nikoperez98@gmail.com>
2025-06-06 22:52:44 -0300

    fix admin

--- src/nav-items.tsx
+++ src/nav-items.tsx
@@ -1,6 +1,9 @@
 
 import { HomeIcon } from "lucide-react";
 import Index from "./pages/Index";
+import Auth from "./pages/Auth";
+import Admin from "./pages/Admin";
+import  ProtectedRoute  from "./components/ProtectedRoute";
 
 export const navItems = [
   {
@@ -9,4 +12,14 @@ export const navItems = [
     icon: <HomeIcon className="h-4 w-4" />,
     page: <Index />,
   },
+  {
+    title: "Auth",
+    to: "/auth",
+    page: <Auth />,
+  },
+  {
+    title: "Admin",
+    to: "/admin",
+    page: <ProtectedRoute><Admin /></ProtectedRoute>,
+  }
 ];
